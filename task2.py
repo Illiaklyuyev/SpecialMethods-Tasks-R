091870bdbe9c428e370f2fb260333d0208410206
Illiaklyuyev <klyuyev218@gmail.com>
2023-04-12 10:38:32 +0000

    fixed task2

--- task2.py
+++ task2.py
@@ -1,5 +1,5 @@
+from math import isnan
 from time import sleep
-from math import factorial
 from mpi4py import MPI
 from random import randint
 
@@ -8,21 +8,22 @@ N=100
 comm=MPI.COMM_WORLD
 size=comm.Get_size()
 rank=comm.Get_rank()
+
 def f(x,rank,size):
-    delay=randint(10,1000)
-    sleep(delay)
+    delay=randint(10,100)
+    sleep(delay/1000)
 
 if rank==0:
-    points=list(map(float,range(0,N-1)))
+    points=list(map(float,range(0,N)))
     results=[]
     for i in range(1,size):
         point=points.pop()
         comm.send(point,i)
-        print("results.len = ", end="")
+    print("results.len = ", end="",flush=True)
     while points:
         free_process=None
         for i in range(1,size):
-            status=comm.improbe(i)
+            status=comm.iprobe(i)
             if status is not None:
                 free_process=i
                 break
@@ -30,18 +31,19 @@ if rank==0:
             continue
         result=comm.recv(source=free_process)
         results.append(result)
-        print(len(result), end=" ")
+        print(len(results), end=" ",flush=True)
         point=points.pop()
         comm.send(point,free_process)
-        for i in range(1,N-1):
-            result=comm.recv()
-            results.append(result)
-            print(len(results), end=" ")
-            comm.send(float("nan"),0)
+    for i in range(1,size):
+        result=comm.recv(source=i)
+        results.append(result)
+        print(len(results), end=" ",flush=True)
+        comm.send(float("nan"),i)
 else:
     while True:
-        point=comm.recv(0)
-        if point==float("nan"):
+        point=comm.recv(source=0)
+        if isnan(point):
             break
         result=f(point,rank,size)
+        comm.send(result,0)
 
